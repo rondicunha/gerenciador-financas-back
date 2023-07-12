27df5a73168573bd3bb85ae9bbfefcb7304a011c
laiana meirinho <rondinelle.cunha.101@ufrn.edu.br>
2023-07-12 08:36:24 -0300

    back-end completo do projeto, ja com sqlite

--- index.js
+++ index.js
@@ -1,45 +1,79 @@
-var express = require('express');
-var app = express();
-var cors = require('cors');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
 
 app.use(cors());
 app.use(express.json());
-itens = [{
-            "descricao": 'Salário',
-            "valor": '1500,00',
-            "tipo": 'Entrada'
-        },
-        {
-            "descricao": 'Cinema',
-            "valor": '50,00',
-            "tipo": 'Saída'
-        }];
 
-    app.delete('/items/:id', function (req, res) {
-        const itemId = req.params.id;
-        itens = itens.filter((item) => item.id !== itemId);
-        return res.sendStatus(200);
-    });    
-           
-app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-})
+const db = new sqlite3.Database('./database.sqlite');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS items (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      descricao TEXT,
+      valor REAL,
+      tipo TEXT
+    )
+  `);
+});
+
+app.delete('/items/:id', function (req, res) {
+  const itemId = req.params.id;
+  db.run('DELETE FROM items WHERE id = ?', [itemId], function(err) {
+    if (err) {
+      console.error('Erro ao excluir o item:', err);
+      return res.sendStatus(500);
+    } else {
+      console.log('Item excluído:', itemId);
+      return res.sendStatus(200);
+    }
+  });
+});
 
 app.get('/', function(req, res){
-    return res.json(itens);
-})
+  db.all('SELECT * FROM items', (err, rows) => {
+    if (err) {
+      console.error('Erro ao ler os itens:', err);
+      return res.sendStatus(500);
+    } else {
+      return res.json(rows);
+    }
+  });
+});
 
 app.post("/cadastro", function (req, res) {
-    const body = req.body;
-    itens.push(body)
-    return res.json(itens)
+  const body = req.body;
+  db.run('INSERT INTO items (descricao, valor, tipo) VALUES (?, ?, ?)', [body.descricao, body.valor, body.tipo], function(err) {
+    if (err) {
+      console.error('Erro ao criar o item:', err);
+      return res.sendStatus(500);
+    } else {
+      const newItem = {
+        id: this.lastID,
+        descricao: body.descricao,
+        valor: body.valor,
+        tipo: body.tipo
+      };
+      return res.json(newItem);
+    }
+  });
 });
 
 app.post("/excluir", function (req, res) {
-    const itemToDelete = req.body;
-    itens = itens.filter((item) => item.descricao !== itemToDelete);
-    return res.json(itens);
+  const itemId = req.body.itemId;
+  db.run('DELETE FROM items WHERE id = ?', [itemId], function(err) {
+    if (err) {
+      console.error('Erro ao excluir o item:', err);
+      return res.sendStatus(500);
+    } else {
+      console.log('Item excluído:', itemId);
+      return res.sendStatus(200);
+    }
   });
+});
 
-
-app.listen(3000)
+app.listen(3000, function () {
+  console.log('Server listening on port 3000');
+});
