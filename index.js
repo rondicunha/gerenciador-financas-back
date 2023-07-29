f824dfad66f9b0a776c0523502c6d711cec72093
Rondinelle Cunha <rondinelle.cunha.101@ufrn.edu.br>
2023-07-28 21:14:13 -0300

    Alterando banco para postgres

--- index.js
+++ index.js
@@ -6,22 +6,20 @@ const sqlite3 = require('sqlite3').verbose();
 app.use(cors());
 app.use(express.json());
 
-const db = new sqlite3.Database('./database.sqlite');
+const { Pool } = require('pg');
 
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS items (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      descricao TEXT,
-      valor REAL,
-      tipo TEXT
-    )
-  `);
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'password',
+  host: 'localhost',
+  port: 5432, 
+  database: 'financasDB',
 });
 
 app.delete('/items/:id', function (req, res) {
   const itemId = req.params.id;
-  db.run('DELETE FROM items WHERE id = ?', [itemId], function(err) {
+  pool.query('DELETE FROM items WHERE id = $1', [itemId], (err, result) => {
     if (err) {
       console.error('Erro ao excluir o item:', err);
       return res.sendStatus(500);
@@ -32,44 +30,28 @@ app.delete('/items/:id', function (req, res) {
   });
 });
 
+// Exemplo de SELECT com PostgreSQL
 app.get('/', function(req, res){
-  db.all('SELECT * FROM items', (err, rows) => {
+  pool.query('SELECT * FROM items', (err, result) => {
     if (err) {
       console.error('Erro ao ler os itens:', err);
       return res.sendStatus(500);
     } else {
-      return res.json(rows);
+      return res.json(result.rows);
     }
   });
 });
 
+// Exemplo de INSERT com PostgreSQL
 app.post("/cadastro", function (req, res) {
   const body = req.body;
-  db.run('INSERT INTO items (descricao, valor, tipo) VALUES (?, ?, ?)', [body.descricao, body.valor, body.tipo], function(err) {
+  pool.query('INSERT INTO items (descricao, valor, tipo) VALUES ($1, $2, $3) RETURNING *', [body.descricao, body.valor, body.tipo], (err, result) => {
     if (err) {
       console.error('Erro ao criar o item:', err);
       return res.sendStatus(500);
     } else {
-      const newItem = {
-        id: this.lastID,
-        descricao: body.descricao,
-        valor: body.valor,
-        tipo: body.tipo
-      };
+      const newItem = result.rows[0];
       return res.json(newItem);
-    }
-  });
-});
-
-app.post("/excluir", function (req, res) {
-  const itemId = req.body.itemId;
-  db.run('DELETE FROM items WHERE id = ?', [itemId], function(err) {
-    if (err) {
-      console.error('Erro ao excluir o item:', err);
-      return res.sendStatus(500);
-    } else {
-      console.log('Item excluído:', itemId);
-      return res.sendStatus(200);
     }
   });
 });
